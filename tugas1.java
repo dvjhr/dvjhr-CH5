1468c1b79e5434beac22f7695705323a00fd4fd7
dvjhr <2dava.aditya21@gmail.com>
2022-11-18 00:42:41 +0700

    edit tugas1.java, wrong file pushed

--- tugas1.java
+++ tugas1.java
@@ -1,119 +1,44 @@
-import java.util.Scanner;
-
-
+/**
+ * challenge
+ */
 public class tugas1 {
+    
     public static void main(String[] args) {
-        switchcase();
-        looping();
-        whileloop();
+        System.out.println(basic(8));
     }
 
-    public static String switchcase() {
-        // switch case function to check odd/even number (integer >0 only)
-        System.out.println("Switch Case Function for Odd/Even Test");
-        // using try so scanner util will be closed automatically after function call ends
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Insert Number: ");
-            int a = sc.nextInt();
-            String x;
+    public static double basic(int x) {
+        // 5 jam pertama = 1
+        // >5 jam = 5 + 0.5x
+        // 24 jam = 15
+        double res;
 
-            // switch condition to check remainder of given number
-            switch (a % 2) {
-                // if there is no remainder -> even number
-                case 0:
-                x = "Genap";
-                System.out.println(x);
-                return x;
+        if (x >= 24) {
+            
+            if (x == 24) {
+
+                res = 15;
+                return res;
+
+            } else {
                 
-                // if there is still a remainder -> odd number
-                case 1:
-                x = "Ganjil";
-                System.out.println(x);
-                return x;
+                //using int to automatically get floor rounding
+                int a = x / 24;
+                double y = x % 24;
+
+                res = (double) a * 15.0 + (y * 0.5);
                 
-                // if there is something else -> error
-                default:
-                    x = "Aneh";
-                    System.out.println(x);
-                    return x;
+                return res;
+                
             }
-             
-        }
-    }
 
-    public static void looping() {
-        String[] days = {"Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu", "Minggu"};
-
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Insert Number of Element to be Printed: ");
-            int jml = sc.nextInt();
-            
-            // calling loopforeach function
-            loopforeach(days);
-            
-            // calling loopxelem function
-            String myarr[] = loopxelem(jml, days);
-
-            // print all element(s) from new array
-            for (String elem : myarr) {
-                System.out.println(elem);
-            }
-        }
-
-    }
-
-    public static void loopforeach(String [] arr) {
-        // print all elements using for each
-        System.out.println("Using For Each to Print All Elements");
-        for (String elem : arr) {
-            System.out.println(elem);
         }
         
+        double y = (x > 5) ? (x - 5) : 0;
+        res = 5 + (y * 0.5);
+                
+        return res;
+
     }
 
-    public static String[] loopxelem(int jml, String [] arr) {
-        // print first x element(s) using manual indexing
-        System.out.println("Printing only " + jml + " First Element(s) from Array");
-        String[] newArr = new String[jml];
-        for (int i = 0; i < jml; i++) {
-            // error handling out of bound array (sometimes unnecesarry)
-            if (i == arr.length) {
-                break;
-            }
-            // System.out.println(arr[i]);
-
-            // copying element from source array to new array 
-            newArr[i] = arr[i];
-         }
-        
-        return newArr;
-        
-    }
-
-    public static void whileloop() {
-        int i = 1;
-
-        // using try so scanner util will be closed automatically after function call ends
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Insert Your Number: ");
-            int myNum = sc.nextInt();
-            
-            System.out.println("Printing Factor of Number" + myNum);
-            while (i <= myNum) {
-
-                // unnecessary error handling
-                if (i > myNum) {
-                    break;
-                }
-
-                // if it is number's factor then print
-                if (myNum % i == 0) {
-                    System.out.println(i);
-                }
-
-                // increment
-                i++;
-            }
-        }
-    }
 }
